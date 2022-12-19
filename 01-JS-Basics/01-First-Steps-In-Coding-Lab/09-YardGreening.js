eb0c7ccd502e0faf8354482a39cdf4eaec2b3b15
VelinIliev <veliniliev@gmail.com>
2022-12-19 15:53:53 +0200

    gitignore

--- 01-JS-Basics/01-First-Steps-In-Coding-Lab/09-YardGreening.js
+++ 01-JS-Basics/01-First-Steps-In-Coding-Lab/09-YardGreening.js
@@ -1,11 +1,10 @@
 function calculatePrice(input) {
     const price_per_sq_m = 7.61;
     let area = input[0]*1;
-    let discount = ((area * price_per_sq_m) * .18).toFixed(2)
-    let final_price = (area * price_per_sq_m  - discount).toFixed(2)
+    let discount = ((area * price_per_sq_m) * .18)
+    let final_price = (area * price_per_sq_m  - discount)
     console.log(`The final price is: ${final_price} lv.`);
     console.log(`The discount is: ${discount} lv.`);
-    
 }
 
 calculatePrice(["550"])
