8c0839fd186f9c6e0882378374542a9fad58a768
VelinIliev <veliniliev@gmail.com>
2022-12-20 17:23:30 +0200

    Conditional Statements Advanced - Exercise

---
+++ 01-JS-Basics/06-ConditionalStatementsAdvanced-Exercise/01-Cinema.js
@@ -0,0 +1,17 @@
+function main(params) {
+    let projection = params[0];
+    let rows = params[1] * 1;
+    let cols = params[2] * 1;
+    let places = rows * cols;
+    let types = {
+        'Premiere': 12,
+        'Normal': 7.5,
+        'Discount': 5,
+    }
+    let total = (places * types[projection]).toFixed(2)
+    console.log(`${total} leva`);
+}
+
+main(["Premiere", "10", "12"]);
+main((["Normal", "21", "13"]));
+main((["Discount", "12", "30"]));
